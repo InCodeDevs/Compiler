ac4fc3fb1c72f1aea37334f44f43e04c67773384
MCTzOCK <dev@mctzock.de>
2021-11-19 16:40:14 +0100

    Typescript Namespaces

---
+++ src/modules/Compiler.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Ben Siebert <ben@mctzock.de>
+ * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
+ */
+
+export class Compiler {
+    public static x() {
+        console.log("b");
+    }
+}
