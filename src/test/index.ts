b19c766308694efd565f268dcf777690af1822ed
MCTzOCK <dev@mctzock.de>
2021-11-26 20:01:03 +0100

    Added Timer

--- src/test/index.ts
+++ src/test/index.ts
@@ -5,4 +5,15 @@
 
 import AST from "./ast";
 
+let startTime, endTime;
+
+startTime = new Date();
+
 new AST().runTest();
+
+endTime = new Date();
+let timeDiff = endTime - startTime;
+timeDiff /= 1000;
+console.log();
+console.log("Time elpased: " + timeDiff + " seconds");
+console.log();
