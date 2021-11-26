76e110c691e9f647145d78af0cc4c73318fe0755
MCTzOCK <dev@mctzock.de>
2021-11-26 20:01:28 +0100

    Fixed Typo

--- src/test/index.ts
+++ src/test/index.ts
@@ -15,5 +15,5 @@ endTime = new Date();
 let timeDiff = endTime - startTime;
 timeDiff /= 1000;
 console.log();
-console.log("Time elpased: " + timeDiff + " seconds");
+console.log("Time elapsed: " + timeDiff + " seconds");
 console.log();
