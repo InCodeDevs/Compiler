4506ad95fd1c541b7b734ba5a2dd8c116e7d2f07
MCTzOCK <dev@mctzock.de>
2021-11-27 08:47:26 +0100

    Updated Tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -21,6 +21,9 @@ endTime = new Date();
 let timeDiff = endTime - startTime;
 timeDiff /= 1000;
 console.log();
+console.log("Time elapsed: " + timeDiff + " seconds");
+console.log();
+
 console.log(
   "Tests PASSED: " +
     (InCodeTest.TOTAL_TESTS - InCodeTest.FAILED_TESTS) +
@@ -28,9 +31,9 @@ console.log(
     InCodeTest.TOTAL_TESTS
 );
 if (InCodeTest.FAILED_TESTS === 0) {
-  console.log(chalk.green("All tests passed.\n"));
+  console.log(chalk.greenBright("All tests passed.\n"));
+  process.exit(0);
 } else {
   console.log(chalk.red("Not all tests passed.\n"));
+  process.exit(1);
 }
-console.log("Time elapsed: " + timeDiff + " seconds");
-console.log();
