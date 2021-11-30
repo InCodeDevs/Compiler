7f0fe925172f6a0e9cbe7176a95bf2c2c356bae4
MCTzOCK <dev@mctzock.de>
2021-11-30 18:49:00 +0100

    Added Building

--- src/modules/errors/InCodeError.ts
+++ src/modules/errors/InCodeError.ts
@@ -2,20 +2,13 @@
  * @author Ben Siebert <ben@mctzock.de>
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
-import chalk from "chalk";
 
 export default class InCodeError {
   constructor(name: string, message: string) {
     try {
       throw new Error("Silent Error");
     } catch (e) {
-      console.log(
-        chalk.bgRed.black(" ERROR ") +
-          "\t\t" +
-          chalk.gray(name) +
-          ": " +
-          message
-      );
+      console.error(" ERROR " + "\t\t" + ": " + message);
     }
   }
 }
